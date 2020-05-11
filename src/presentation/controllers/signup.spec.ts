fe65d9c69b948f6c3886181f3bd59250330a0f35
Danilo Rodrigues Catapan <danilo.catapan@somospi.com.br>
2020-05-11 17:40:41 -0300

    test: ensure SignUpController return an error if no name is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -12,5 +12,6 @@ describe('SignUp Controller', () => {
     }
     const httpResponde = sut.handle(httpRequest)
     expect(httpResponde.statusCode).toBe(400)
+    expect(httpResponde.body).toEqual(new Error('Missing param: name')) // toEqual validate the value
   })
 })
