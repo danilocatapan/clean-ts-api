ce09fbed752d422aecc0c84eb2b3c7ed9da38630
Danilo Rodrigues Catapan <danilo.catapan@somospi.com.br>
2020-05-12 16:22:45 -0300

    feat: ensure DbAddAccount calls AddAccountRepository with correct values

---
+++ src/data/protocols/add-account-repository.ts
@@ -0,0 +1,6 @@
+import { AddAccountModel } from '../../domain/usercases/add-account'
+import { AccountModel } from '../../domain/models/account'
+
+export interface AddAccountRepository {
+  add (accountData: AddAccountModel): Promise<AccountModel>
+}
