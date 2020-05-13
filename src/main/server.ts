c3c188037943cf4ff139255dc2bb8cd1065992b5
Danilo Rodrigues Catapan <danilo.catapan@somospi.com.br>
2020-05-13 10:23:07 -0300

    feat: connect to mongodb before app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,10 @@
-import app from './config/app'
 
-app.listen(5050, () => console.log('Server running at http://localhost:5050'))
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
+import env from './config/env'
+
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+  })
+  .catch(console.error)
