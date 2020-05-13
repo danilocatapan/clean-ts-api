612301ac400a491b9b8f5f0ed950d5194589f7c6
Danilo Rodrigues Catapan <danilo.catapan@somospi.com.br>
2020-05-13 15:03:54 -0300

    refactor: make server error return the error stack

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -35,7 +35,7 @@ export class SignUpController implements Controller {
       })
       return ok(account)
     } catch (error) {
-      return serverError()
+      return serverError(error)
     }
   }
 }
