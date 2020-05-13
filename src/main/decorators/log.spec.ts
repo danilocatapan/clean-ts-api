fd8400f03db2c76d4feadbe96453273db6f2acc9
Danilo Rodrigues Catapan <danilo.catapan@somospi.com.br>
2020-05-13 14:45:37 -0300

    test: ensure LogControllerDecorator calls controller with correct value

---
+++ src/main/decorators/log.spec.ts
@@ -0,0 +1,31 @@
+import { LogControllerDecorator } from './log'
+import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
+
+describe('LogController Decorator', () => {
+  test('Should call controller handle', async () => {
+    class ControllerStub implements Controller {
+      async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+        const httpResponse: HttpResponse = {
+          statusCode: 200,
+          body: {
+            name: 'Danilo'
+          }
+        }
+        return new Promise(resolve => resolve(httpResponse))
+      }
+    }
+    const controllerStub = new ControllerStub()
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+    const sut = new LogControllerDecorator(controllerStub)
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    await sut.handle(httpRequest)
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+})
