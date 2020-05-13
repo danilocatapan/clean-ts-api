485bb45c3afb1545e1a584790cd6a8c37eb4219e
Danilo Rodrigues Catapan <danilo.catapan@somospi.com.br>
2020-05-13 11:32:14 -0300

    refactor: name passwordconfirmation

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -13,16 +13,16 @@ export class SignUpController implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const requiredFields = ['name', 'email', 'password', 'passwordConfirmaton']
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
           return badRequest(new MissingParamError(field))
         }
       }
 
-      const { name, email, password, passwordConfirmaton } = httpRequest.body
-      if (password !== passwordConfirmaton) {
-        return badRequest(new InvalidParamError('passwordConfirmaton'))
+      const { name, email, password, passwordConfirmation } = httpRequest.body
+      if (password !== passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
       }
       const isValid = this.emailValidator.isValid(email)
       if (!isValid) {
