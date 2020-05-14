b30045d1a8294c06ac4b541dace436523e0c14e7
Danilo Rodrigues Catapan <danilo.catapan@somospi.com.br>
2020-05-14 18:40:31 -0300

    test: ensure DbAuthentication throws if UpdateAccessTokenRepository throws

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -150,4 +150,11 @@ describe('DbAuthentication UseCase', () => {
     await sut.auth(makeFaceAuthentication())
     expect(updateSpy).toHaveBeenCalledWith('any_id', 'any_token')
   })
+
+  test('Should throw if UpdateAccessTokenRepository throws', async () => {
+    const { sut, updateAccessTokenRepositoryStub } = makeSut()
+    jest.spyOn(updateAccessTokenRepositoryStub, 'update').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.auth(makeFaceAuthentication())
+    await expect(promise).rejects.toThrow()
+  })
 })
