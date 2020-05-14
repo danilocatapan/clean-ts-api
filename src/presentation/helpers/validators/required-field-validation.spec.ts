4cfef3152fe662dccdba24270ca52531dff40802
Danilo Rodrigues Catapan <danilo.catapan@somospi.com.br>
2020-05-14 13:22:34 -0300

    test: ensure RequiredFieldValidation not return if validation succeds

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -3,8 +3,14 @@ import { MissingParamError } from '../../errors'
 
 describe('RequiredField Validation', () => {
   test('Should return a MissingParamError if validation fails', () => {
-    const sut = new RequiredFieldValidation('any_field')
+    const sut = new RequiredFieldValidation('field')
     const error = sut.validate({ name: 'any_name' })
-    expect(error).toEqual(new MissingParamError('any_field'))
+    expect(error).toEqual(new MissingParamError('field'))
+  })
+
+  test('Should not return if validation succeds', () => {
+    const sut = new RequiredFieldValidation('field')
+    const error = sut.validate({ field: 'any_name' })
+    expect(error).toBeFalsy()
   })
 })
