209918daeb58bbe63ca0429159feee926cd73578
Danilo Rodrigues Catapan <danilo.catapan@somospi.com.br>
2020-05-14 18:08:48 -0300

    test: ensure DbAuthentication returns null if HashComparer returns false

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -86,4 +86,11 @@ describe('DbAuthentication UseCase', () => {
     const promise = sut.auth(makeFaceAuthentication())
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return null if HashComparer returns false', async () => {
+    const { sut, hashComparerStub } = makeSut()
+    jest.spyOn(hashComparerStub, 'compare').mockReturnValueOnce(new Promise(resolve => resolve(false)))
+    const acessToken = await sut.auth(makeFaceAuthentication())
+    expect(acessToken).toBeNull()
+  })
 })
