7e32b9b0b6b97ca245bfc64e5723c8750f05f170
Danilo Rodrigues Catapan <danilo.catapan@somospi.com.br>
2020-05-15 08:36:32 -0300

    refactor: improve file names

--- src/data/protocols/db/add-account-repository.ts
+++
@@ -1,6 +0,0 @@
-import { AddAccountModel } from '../../../domain/usercases/add-account'
-import { AccountModel } from '../../../domain/models/account'
-
-export interface AddAccountRepository {
-  add (accountData: AddAccountModel): Promise<AccountModel>
-}
--- src/main/factories/signup/signup-validation-factory.ts
+++ src/main/factories/signup/signup-validation-factory.ts
@@ -1,12 +1,13 @@
-import { ValidationComposite, RequiredFieldValidation, EmailValidation } from '../../../presentation/helpers/validators/'
+import { ValidationComposite, RequiredFieldValidation, CompareFieldsValidation, EmailValidation } from '../../../presentation/helpers/validators'
 import { Validation } from '../../../presentation/protocols/validation'
 import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
 
-export const makeLoginValidation = (): ValidationComposite => {
+export const makeSignUpValidation = (): ValidationComposite => {
   const validations: Validation[] = []
-  for (const field of ['email', 'password']) {
+  for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
     validations.push(new RequiredFieldValidation(field))
   }
+  validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
   validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
   return new ValidationComposite(validations)
 }
