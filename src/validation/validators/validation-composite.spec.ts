2a0b8344515ff50b14eca8303d25b4daf77bc9c5
Danilo Rodrigues Catapan <danilo.catapan@somospi.com.br>
2020-05-16 17:16:39 -0300

    refactor: move validation to its own layer

--- src/validation/validators/validation-composite.spec.ts
+++ src/validation/validators/validation-composite.spec.ts
@@ -1,5 +1,5 @@
-import { Validation } from '../../protocols/validation'
-import { MissingParamError } from '../../errors'
+import { Validation } from '../../presentation/protocols'
+import { MissingParamError } from '../../presentation/errors'
 import { ValidationComposite } from './validation-composite'
 
 const makeValidation = (): Validation => {
