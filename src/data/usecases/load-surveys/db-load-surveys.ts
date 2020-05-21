de700ac4cad2a7bfb1a3a24d9035ae9bb9e1fefa
Danilo Rodrigues Catapan <danilo.catapan@somospi.com.br>
2020-05-21 16:00:31 -0300

    feat: ensure DbLoadSurveys calls LoadSurveysRepository

---
+++ src/data/usecases/load-surveys/db-load-surveys.ts
@@ -0,0 +1,12 @@
+import { LoadSurveys } from '../../../domain/usercases/load-surveys'
+import { SurveyModel } from '../../../domain/models/survey'
+import { LoadSurveysRepository } from '../../protocols/db/survey/load-survey-repository'
+
+export class DbLoadSurveys implements LoadSurveys {
+  constructor (private readonly loadSurveysRespository: LoadSurveysRepository) {}
+
+  async load (): Promise<SurveyModel[]> {
+    await this.loadSurveysRespository.loadAll()
+    return []
+  }
+}
