f1236cb165c63c7288e14a8d5b36ccea7602ed32
Danilo Rodrigues Catapan <danilo.catapan@somospi.com.br>
2020-05-21 14:44:08 -0300

    refactor: change AddSurvey to insert a date also

--- src/data/usecases/add-survey/db-add-survey.spec.ts
+++ src/data/usecases/add-survey/db-add-survey.spec.ts
@@ -1,12 +1,14 @@
 import { DbAddSurvey } from './db-add-survey'
 import { AddSurveyModel, AddSurveyRepository } from './db-add-survey-protocols'
+import MockDate from 'mockdate'
 
 const makeFakeSurveyData = (): AddSurveyModel => ({
   question: 'any_question',
   answers: [{
     image: 'any_image',
     answer: 'any_answer'
-  }]
+  }],
+  date: new Date()
 })
 
 const makeAddSurveyRepository = (): AddSurveyRepository => {
@@ -33,6 +35,14 @@ const makeSut = (): SutTypes => {
 }
 
 describe('DbAddSurvey UserCase', () => {
+  beforeAll(() => {
+    const date = new Date()
+    MockDate.set(date.getTime())
+  })
+
+  beforeAll(() => {
+    MockDate.reset()
+  })
   test('Should call AddSurveyRepository with corret values', async () => {
     const { sut, addSurveyRepositoyStub } = makeSut()
     const addSpy = jest.spyOn(addSurveyRepositoyStub, 'add')
--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -1,6 +1,7 @@
 import { HttpRequest, AddSurvey, AddSurveyModel, Validation } from './add-survey-controller-protocols'
 import { AddSurveyController } from './add-survey-controller'
 import { badRequest, serverError, noContent } from '../../../helpers/http/http-helper'
+import MockDate from 'mockdate'
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
@@ -8,7 +9,8 @@ const makeFakeRequest = (): HttpRequest => ({
     answers: [{
       image: 'any_image',
       answer: 'any_answer'
-    }]
+    }],
+    date: new Date()
   }
 })
 
@@ -47,6 +49,15 @@ const makeSut = (): SutTypes => {
 }
 
 describe('AddSurvey Controller', () => {
+  beforeAll(() => {
+    const date = new Date()
+    MockDate.set(date.getTime())
+  })
+
+  beforeAll(() => {
+    MockDate.reset()
+  })
+
   test('Should call Validation with correct values', async () => {
     const { sut, validationStub } = makeSut()
     const validateSpy = jest.spyOn(validationStub, 'validate')
