d4a3b6f6a6f8357930d5c27a6a6669c2db662796
Danilo Rodrigues Catapan <danilo.catapan@somospi.com.br>
2020-05-22 15:01:04 -0300

    feat: ensure DbLoadSurveyById calls LoadSurveyByIdRepository with corret id

--- src/data/protocols/db/survey/load-survey-by-id-repository.ts
+++ src/data/protocols/db/survey/load-survey-by-id-repository.ts
@@ -1,5 +1,5 @@
 import { SurveyModel } from '@/domain/models/survey'
 
-export interface LoadSurveyById {
+export interface LoadSurveyByIdRepository {
   loadById (id: string): Promise<SurveyModel>
 }
