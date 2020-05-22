2d2767772e27577a7fb3f3f4b5c598c1f336b187
Danilo Rodrigues Catapan <danilo.catapan@somospi.com.br>
2020-05-22 18:23:22 -0300

    fix: ensure SurveyMongoRepository uses MongoHelper.map in all methods

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -6,20 +6,20 @@ import { LoadSurveysRepository } from '@/data/protocols/db/survey/load-survey-re
 import { LoadSurveyByIdRepository } from '@/data/protocols/db/survey/load-survey-by-id-repository'
 
 export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository, LoadSurveyByIdRepository {
-  async add (surrveyData: AddSurveyModel): Promise<void> {
+  async add (surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
-    await surveyCollection.insertOne(surrveyData)
+    await surveyCollection.insertOne(surveyData)
   }
 
   async loadAll (): Promise<SurveyModel[]> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
     const surveys = await surveyCollection.find().toArray()
-    return surveys
+    return MongoHelper.mapCollection(surveys)
   }
 
   async loadById (id: string): Promise<SurveyModel> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
     const survey = await surveyCollection.findOne({ _id: id })
-    return survey
+    return survey && MongoHelper.map(survey)
   }
 }
