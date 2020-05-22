abe1419c67fe8252bd28150b572559934fcd0406
Danilo Rodrigues Catapan <danilo.catapan@somospi.com.br>
2020-05-22 11:59:46 -0300

    refactor: change jest mock to use path modules

--- src/main/factories/controllers/login/signup/signup-validation-factory.spec.ts
+++ src/main/factories/controllers/login/signup/signup-validation-factory.spec.ts
@@ -3,7 +3,7 @@ import { ValidationComposite, RequiredFieldValidation, CompareFieldsValidation, 
 import { Validation } from '@/presentation/protocols/validation'
 import { EmailValidator } from '@/validation/protocols/email-validator'
 
-jest.mock('../../../../../validation/validators/validation-composite')
+jest.mock('@/validation/validators/validation-composite')
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator { // Stub: force specific return
