94cfdfc3c6699d29edb4fd1419e1b0f5030b4961
Danilo Rodrigues Catapan <danilo.catapan@somospi.com.br>
2020-05-25 11:23:40 -0300

    test: ensure save survey result route returns 403 if no token provided

---
+++ src/main/routes/survey-result-routes.test.ts
@@ -0,0 +1,25 @@
+import app from '@/main/config/app'
+import env from '@/main/config/env'
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import request from 'supertest'
+
+describe('Survey Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(env.mongoUrl)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  describe('PUT /surveys/:surveyId/results', () => {
+    test('Should return 403 on save survey result without accessToken', async () => {
+      await request(app)
+        .put('/api/surveys/any_id_/results')
+        .send({
+          answer: 'any_answer'
+        })
+        .expect(403)
+    })
+  })
+})
