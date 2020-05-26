8c7910706f702ac58bbb92d34bf6756561275c39
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-05-26 08:41:18 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    description: 'Essa é a documentação da API feita pelo instrutor Rodrigo Manguinho no curso da Udemy de NodeJs usando Typescript, TDD, Clean Architecture e seguindo os princípios do SOLID e Design Patterns.',
+    version: '1.0.0'
+  }
+}
