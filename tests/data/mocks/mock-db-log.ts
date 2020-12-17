5ae7f00fc477991d337052dbdcbd8082dd02a31a
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-12-17 18:02:04 -0300

    chore: remove tests/data to gitignore

---
+++ tests/data/mocks/mock-db-log.ts
@@ -0,0 +1,9 @@
+import { LogErrorRepository } from '@/data/protocols/db/log/log-error-repository'
+
+export class LogErrorRepositorySpy implements LogErrorRepository {
+  stack: string
+
+  async logError (stack: string): Promise<void> {
+    this.stack = stack
+  }
+}
