c48f1c2f9e7be217924fbcb760e1d97068102ea4
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-12-21 16:24:45 -0300

    refactor: add types to LoadSurveys usecase

--- src/data/usecases/db-load-surveys.ts
+++ src/data/usecases/db-load-surveys.ts
@@ -1,12 +1,11 @@
 import { LoadSurveys } from '@/domain/usecases'
-import { SurveyModel } from '@/domain/models'
 import { LoadSurveysRepository } from '@/data/protocols'
 
 export class DbLoadSurveys implements LoadSurveys {
-  constructor (private readonly loadSurveysRespository: LoadSurveysRepository) {}
+  constructor (private readonly loadSurveysRepository: LoadSurveysRepository) {}
 
-  async load (accountId: string): Promise<SurveyModel[]> {
-    const surveys = await this.loadSurveysRespository.loadAll(accountId)
+  async load (accountId: string): Promise<LoadSurveys.Result> {
+    const surveys = await this.loadSurveysRepository.loadAll(accountId)
     return surveys
   }
 }
