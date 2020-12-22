9ee23e07ba92d7d097e32e0514ca0b2c4632a57c
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-12-22 08:31:12 -0300

    feat: add status code error on exceptions

--- src/main/adapters/apollo-resolver-route-adapter.ts
+++ src/main/adapters/apollo-resolver-route-adapter.ts
@@ -1,6 +1,14 @@
 import { Controller } from '@/presentation/protocols'
+import { UserInputError, AuthenticationError, ForbiddenError, ApolloError } from 'apollo-server-express'
 
 export const adaptResolver = async (controller: Controller, args: any): Promise<any> => {
   const httpResponse = await controller.handle(args)
-  return httpResponse.body
+  switch (httpResponse.statusCode) {
+    case 200:
+    case 204: return httpResponse.body
+    case 400: return new UserInputError(httpResponse.body.message)
+    case 401: return new AuthenticationError(httpResponse.body.message)
+    case 403: return new ForbiddenError(httpResponse.body.message)
+    default: return new ApolloError(httpResponse.body.message)
+  }
 }
