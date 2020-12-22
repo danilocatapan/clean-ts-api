f6452ecc89b07700f9c5f82dbcc9259a6dbb567a
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-12-22 08:40:33 -0300

    feat: add signUp mutation

--- src/main/graphql/type-defs/login.ts
+++ src/main/graphql/type-defs/login.ts
@@ -4,6 +4,11 @@ export default gql`
   extend type Query {
     login (email: String!, password: String!): Account!
   }
+
+  extend type Mutation {
+    singUp (name: String!, email: String!, password: String!, passwordConfirmation: String!): Account!
+  }
+
   type Account {
     accessToken: String!
     name: String!
