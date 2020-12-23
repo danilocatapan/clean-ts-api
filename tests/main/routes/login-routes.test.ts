095648a3537a96318f13aee14eee7e56f8aa1c3d
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-12-23 09:52:48 -0300

    refactor: organize code

--- tests/main/routes/login-routes.test.ts
+++ tests/main/routes/login-routes.test.ts
@@ -1,5 +1,5 @@
 import app from '@/main/config/app'
-import { MongoHelper } from '@/infra/db/mongodb/mongo-helper'
+import { MongoHelper } from '@/infra/db'
 
 import { Collection } from 'mongodb'
 import { hash } from 'bcrypt'
